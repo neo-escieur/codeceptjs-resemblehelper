26969bd03bb44a8977540493adde5d331df1c0db
escieur <emmanuel.scieur@ac-orleans-tours.fr>
2026-09-15 19:26:47 +0200

    fix: visuals tests

--- __test__/visual_test.ts
+++ __test__/visual_test.ts
@@ -2,15 +2,15 @@ const { I } = inject()
 Feature('visual tests');
 
 Before(() => {
-    I.amOnPage('https://codecept.io/Playwright/')
+    I.amOnPage('https://codecept.io/playwright/')
 })
 
 Scenario('seeVisualDiff',  () => {
     I.saveScreenshot('Playwright_doc.png');
-    I.seeVisualDiff('Playwright_doc.png', {prepareBaseImage: true, tolerance: 20})
+    I.seeVisualDiff('Playwright_doc.png', {prepareBaseImage: false, tolerance: 20})
 });
 
 Scenario('seeVisualDiffForElement',  () => {
-    I.saveElementScreenshot('h2#playwright','element.png');
-    I.seeVisualDiffForElement('h2#playwright','element.png', {prepareBaseImage: true, tolerance: 20})
+    I.saveElementScreenshot('h1#_top','element.png');
+    I.seeVisualDiffForElement('h1#_top','element.png', {prepareBaseImage: false, tolerance: 20})
 });
