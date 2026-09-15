f0af4790b45d6fbb9998c2672f6e342a6954c41b
escieur <emmanuel.scieur@ac-orleans-tours.fr>
2026-09-15 17:18:03 +0200

    ci: tests prepare base images

--- __test__/visual_test.ts
+++ __test__/visual_test.ts
@@ -2,15 +2,15 @@ const { I } = inject()
 Feature('visual tests');
 
 Before(() => {
-    I.amOnPage('https://codecept.io/helpers/Playwright/')
+    I.amOnPage('https://codecept.io/Playwright/')
 })
 
 Scenario('seeVisualDiff',  () => {
     I.saveScreenshot('Playwright_doc.png');
-    I.seeVisualDiff('Playwright_doc.png', {prepareBaseImage: false, tolerance: 20})
+    I.seeVisualDiff('Playwright_doc.png', {prepareBaseImage: true, tolerance: 20})
 });
 
 Scenario('seeVisualDiffForElement',  () => {
     I.saveElementScreenshot('h2#playwright','element.png');
-    I.seeVisualDiffForElement('h2#playwright','element.png', {prepareBaseImage: false, tolerance: 20})
+    I.seeVisualDiffForElement('h2#playwright','element.png', {prepareBaseImage: true, tolerance: 20})
 });
